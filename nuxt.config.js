f216ffc88fb394f5aa307e5d6ff88fa826802a07
erygn <etienne.reygner@gmail.com>
2021-01-11 22:24:13 +0100

    Popular Recipies

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,16 +4,38 @@ export default {
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'cocktail-idea',
+    __dangerouslyDisableSanitizers: ['script'],
+    htmlAttrs: {
+      lang: 'en'
+    },
+    title: 'AppHero - Random cocktail',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'keywords', content: 'whisky, run, champagne, vodka, gin'},
+      { hid: 'description', name: 'description', content: 'Random cocktail' },
+      { hid: 'og:description', property: 'og:description', name: 'og:description', content: 'Random cocktail' },
+      { hid: 'twitter:description', property: 'twitter:description', name: 'twitter:description', content: 'Random cocktail' },
+      { hid: 'og:type', property: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', name: 'og:url', content: 'http://apphero.rakura.fr/' },
+      { hid: 'og:title', property: 'og:title', name: 'og:title', content: 'AppHero - Random cocktail' },
+      { hid: 'twitter:site', name: 'twitter:site', content: 'apphero.rakura.fr' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: 'AppHero' },
+      { hid: 'twitter:title', property: 'twitter:title', name: 'twitter:title', content: 'AppHero - Random cocktail' },
+      { hid: 'og:site_name', property: 'og:site_name', name: 'og:site_name', content: 'apphero.rakura.fr' },
+      { hid: 'og:locale', property: 'og:locale', name: 'og:locale', content: 'fr' },
+      { hid: 'og:image', property: 'og:image', name: 'og:image', content: 'http://apphero.rakura.fr/favicon.ico' },
+      { hid: 'twitter:image', property: 'twitter:image', name: 'twitter:image', content: 'http://apphero.rakura.fr/favicon.ico' },
+      { hid: 'twitter:card', property: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'msapplication-TileColor', content: '#F5F5F5' },
+      { name: 'theme-color', content: '#F5F5F5' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
+  loading: { color: '#252525' },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,6 @@
             dense
             label="Search"
             color="#252525"
-            clearable
           />
         </div>
         <div @click="goSearch" class="searchMagnify">
@@ -31,16 +30,36 @@
         </v-chip-group>
       </v-row>
 
-      <div v-if="searchShow === null" class="showNewCocktail">
+      <div v-if="searchShow === null && inputSearch.length == 0 || inputSearch.length == 0 && searchByIngredient == false" class="showNewCocktail">
         <v-img
           src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg"
           class="image-pub"
         >
         <h2 class="pub-title">Find the best cocktail recipies!</h2>
         </v-img>
+
+        <v-img
+        @click="$router.push({name: 'cocktail-cocktail', params: {cocktail: '11000'}})"
+          src="https://www.thecocktaildb.com//images//media//drink//metwgh1606770327.jpg"
+          class="image-pub image-recipies"
+        >
+        <h2 class="pub-title">Mojito</h2>
+        </v-img>
+
+        <v-img
+        @click="$router.push({name: 'cocktail-cocktail', params: {cocktail: '14167'}})"
+          src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/qyxrqw1439906528.jpg"
+          class="image-pub image-recipies"
+        >
+        <h2 class="pub-title">Vodka Martini</h2>
+        </v-img>
       </div>
 
-      <div v-if="searchShow != null">
+      <div v-if="searchShow === null && lastinput != 0">
+        <p class="notFound">No cocktail found</p>
+      </div>
+
+      <div v-if="searchShow != null && inputSearch.length != 0 || searchShow != null && searchByIngredient != false" class="searchClass">
         <div class="itemIter" v-for="(item, index) in searchShow" :key="index" 
         @click="
               $router.push({
@@ -58,25 +77,9 @@
             />
           </div>
           <div class="itemName">
-            <h2 class="itemTitle">{{ item.strDrink }}</h2>
-            <v-chip-group class="chipGroup" v-if="searchByIngredient == false">
-              <v-chip 
-              small class="chipElem"
-              >{{ item.strAlcoholic }}</v-chip>
-              <v-chip
-                small
-                v-if="item.strIngredient1.length < 10"
-                >{{ item.strIngredient1 }}</v-chip
-              >
-              <v-chip
-                small
-                v-else
-                >{{ item.strIngredient1.substring(0, 10) }}...</v-chip
-              >
-            </v-chip-group>
-            <v-chip-group class="chipGroup" v-else>
-              <v-chip small class="chipElem">{{ actualIngredient }}</v-chip>
-            </v-chip-group>
+            <h2 class="itemTitle">{{ item.strDrink }} <span v-if="searchByIngredient == false" class="itemAlcol">- {{ item.strAlcoholic }}</span></h2>
+            <p v-if="searchByIngredient == false"><span class="itemIngre">{{ item.strIngredient1 }}</span></p>
+            <p v-else><span class="itemIngre">{{ actualIngredient }}</span></p>
           </div>
           <div
            
@@ -246,6 +249,7 @@ export default {
 }
 .itemTitle {
   font-size: 15px;
+  margin-bottom: 2px;
 }
 .itemName {
   flex: 1;
@@ -269,6 +273,7 @@ export default {
 
 .showNewCocktail {
   margin: 10px;
+  padding-bottom: 40px;
   border-radius: 30px;
 }
 
@@ -333,9 +338,13 @@ export default {
   margin-right: auto;
   margin-left: auto;
 }
-
+.image-recipies {
+  height: 150px;
+}
 .image-pub {
+  background: rgba(0,0,0,0.3);
   border-radius: 30px;
+  margin-bottom: 20px;
   display: flex;
   justify-content: center;
   align-items: flex-end;
@@ -345,5 +354,22 @@ export default {
 .pub-title {
   color: white !important
 }
-
+.searchClass {
+  margin-bottom: 60px;
+}
+.itemAlcol {
+  font-weight: 300;
+  font-size: 14px;
+}
+.itemIngre {
+  background-color: #E5E5E5;
+  padding: 5px 10px 5px 10px;
+  border-radius: 20px;
+  font-size: 13px;
+}
+.notFound {
+  margin-top: 20px;
+  font-size: 14px;
+  text-align: center;
+}
 </style>
